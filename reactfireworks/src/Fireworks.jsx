0ea3153c1c5d32e9e2073e4d73bd3f191f3246c9
katelynsmacfarlane <160673815+katelynsmacfarlane@users.noreply.github.com>
2024-03-26 11:08:04 -0700

    Add project files.

---
+++ reactfireworks/src/Fireworks.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+
+function Fireworks() {
+    const [count, setCount] = useState(0);
+    
+    return (
+        <>
+            <div id="app">
+                <center>
+                    <br />
+                    <h3><i>Click anywhere to light the fuse.</i></h3>
+                </center>
+            </div>
+        </>
+    )
+}
+
+// A list of all possible colors
+const COLORS = [["purple", "blue", "lightblue"], ["green", "yellow", "lightgreen"], ["burgundy", "pink", "red"], ["orange", "lightorange", "orange"], ["red", "blue", "white"]];
+// Defines the particle number
+const PARTICLES_NUMBER = 100;
+function createParticle(x, y, firstIndex) {
+    const element = document.createElement("div");
+    element.style.width = "3px";
+    element.style.height = "3px";
+    //element.style.border = "1px solid black";
+    // The elements are in absolute position
+    element.style.position = "absolute";
+    element.style.top = `${y}px`;
+    element.style.left = `${x}px`;
+    // We want our cursor to be centered in the square
+    element.style.transform = "translate(-50%, -50%)";
+    // Get a color randomly
+    console.log(firstIndex);
+    element.style.backgroundColor = //COLORS[0][0];
+        COLORS[firstIndex][Math.floor(Math.random() * 3)];
+
+    const animation = element.animate(
+        [
+            {
+                // Math.random() - 0.5 returns integer between -0.5 and 0.5
+                transform: `translate(${(Math.random() - 0.5) * 500}px, ${(Math.random() - 0.5) * 500
+                    }px) rotate(${Math.random() * 520}deg)`,
+                // We want to reduce the opacity until 0
+                opacity: 0
+            }
+        ],
+        1500
+    );
+
+    // Remove the particle at the end of animation
+    animation.finished.then(() => element.remove());
+
+    document.getElementById('app').appendChild(element);
+}
+
+document.body.addEventListener("click", (e) => {
+    // Get the position of the cursor in the document
+    const { clientX: x, clientY: y } = e;
+    const firstIndex = Math.floor(Math.random() * 5);
+    // Create multiple particles
+    for (let i = 0; i < PARTICLES_NUMBER; i++) {
+        createParticle(x, y, firstIndex);
+    }
+});
+
+export default Fireworks
---
+++ reactfireworks/src/assets/fireworks-click/README.md
@@ -0,0 +1,4 @@
+# Fireworks Click
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Katelyn-MacFarlane-the-animator/pen/bGZJjKM](https://codepen.io/Katelyn-MacFarlane-the-animator/pen/bGZJjKM).
+
